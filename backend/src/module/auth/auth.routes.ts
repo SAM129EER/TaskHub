aad6712a2e113c5be7c48ff6e17595061a4f0e94
Sameer <jangidsameer77@gmail.com>
2026-05-23 19:29:45 +0530

    feat(auth): add validation middleware for authentication routes

--- backend/src/module/auth/auth.routes.ts
+++ backend/src/module/auth/auth.routes.ts
@@ -1,17 +1,12 @@
-import {Router } from "express"
-import { signInController, signUpController} from "./auth.controller.js"
-import { asyncHandler} from "../../utils/asyncHandler.js"
-
+import { Router } from "express";
+import { signInController, signUpController } from "./auth.controller.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
+import { validate } from "../../middleware/validate.middleware.js";
+import { signInSchema } from "./auth.schema.js";
 const router = Router();
 
-router.post(
-  "/sign-up",
-  asyncHandler(signUpController)
-);
+router.post("/sign-up", validate(signInSchema), asyncHandler(signUpController));
 
-router.post(
-  "/sign-in",
-  asyncHandler(signInController)
-);
+router.post("/sign-in", validate(signInSchema), asyncHandler(signInController));
 
-export default router ; 
+export default router;
