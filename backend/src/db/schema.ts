d0b07acee8e5bcd00762a14a870a7f178f8f97e0
Sameer <jangidsameer77@gmail.com>
2026-05-23 12:52:49 +0530

    feat(db): redefine users and sessions tables with UUIDs and additional fields

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,43 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  emailVerified: boolean("email_verified").default(false).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const sessions = pgTable("sessions", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  userId: uuid("user_id")
+    .references(() => usersTable.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+
+  refreshToken: varchar("refresh_token", {
+    length: 500,
+  }).notNull(),
+
+  userAgent: varchar("user_agent", {
+    length: 500,
+  }),
+
+  ipAddress: varchar("ip_address", {
+    length: 100,
+  }),
+
+  expiresAt: timestamp("expires_at").notNull(),
+
+  createdAt: timestamp("created_at").defaultNow(),
 });
